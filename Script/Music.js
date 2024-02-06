c1ae03c4c5012bda3858585f43bd1a7c3b43de61
dextortheclown <dexterwong0724@gmail.com>
2024-02-06 14:22:03 +0800

    update to music

--- Script/Music.js
+++
@@ -1,8 +0,0 @@
-window.onSpotifyIframeApiReady = (IFrameAPI) => {
-    const element = document.getElementById('embed-iframe');
-    const options = {
-        uri: ''
-      };
-    const callback = (EmbedController) => {};
-    IFrameAPI.createController(element, options, callback);
-  };
